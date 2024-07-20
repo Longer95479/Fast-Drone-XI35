1c4c6b8b5978ea3851629d13c55ff01eac303d55
bkbk_b <1320832053@qq.com>
2024-07-20 22:14:57 +0800

    add docker

---
+++ Docker/Dockerfile/container_run.sh
@@ -0,0 +1,7 @@
+docker run -itd --privileged=true --network host \
+        --mount type=bind,source=${HOME}/Docker_Data,target=/root/data \
+        --mount type=bind,source=/dev,target=/dev \
+        --mount source=Fast-Drone-XI35,target=/root/Fast-Drone-XI35 \
+        --runtime=nvidia --gpus all \
+        --name fd_runtime \
+        fastdronexi35:orin /bin/bash
