7abcdce827afc1f052933b017789d7180a6b00a8
bkbk_b <1320832053@qq.com>
2024-07-24 17:29:49 +0800

    update execStartStage

--- src/auto_search/search_plan/include/search_plan_fsm.h
+++ src/auto_search/search_plan/include/search_plan_fsm.h
@@ -17,7 +17,7 @@ namespace auto_search
     {
       STARTING_AREA_STAGE,
       SEARCH_STAGE,
-      LANDING_STAGE
+      LAND_STAGE
     };
 		enum START_SUB_STATE
 		{
@@ -29,9 +29,9 @@ namespace auto_search
 		{
 			SEARCH_NUMS,
 			SLOWDOWN_FOR_RECOG,
-			WAIT_FOR_LANDING
+			WAIT_FOR_LAND
 		};
-		enum LANDING_SUB_STATE
+		enum LAND_SUB_STATE
 		{
 			FLY_TO_NUMS,
 			FINE_TUNE,
@@ -41,25 +41,39 @@ namespace auto_search
 		MAIN_STATE main_State;
 		START_SUB_STATE start_SubState;
 		SEARCH_SUB_STATE search_SubState;
-		LANDING_SUB_STATE landing_SubState;
+		LAND_SUB_STATE land_SubState;
 		
 		//ros
 		ros::Publisher pub_Target;
 		ros::Subscriber sub_Odom;
+		ros::Subscriber sub_Trigger;
 		ros::Timer timer_FSM;
+		//param
+		std::string odom_Topic;
+		double exec_Frequency, arrive_Threshold;
 		//main var
+		bool have_trigger  = false;
 		Eigen::Vector3d current_Target;
 		Eigen::Vector3d last_Target;
 		Eigen::Vector3d current_Position;
+		Eigen::Vector3d search_StartPoint;
+
 
 		//callback
 		void execFSMCallback(const ros::TimerEvent &e);
 		void updateOdomCallback(const nav_msgs::OdometryConstPtr &msg);
+		void triggerCallback(const geometry_msgs::PoseStampedPtr &msg);
 		//function
 		void publishTarget();//发布飞行目标点,只有目标点跟上次不一样时才发布
 		void execStartStage();//执行起始阶段状态机
 		void execSearchStage();//执行搜索阶段状态机
 		void execLandStage();//执行降落阶段状态机
+		bool haveArrivedTarget();//是否到达当前目标点
+		void changeMainState(MAIN_STATE next_state);
+		void changeStartSubState(START_SUB_STATE next_state);
+		void changeSearchSubState(SEARCH_SUB_STATE next_state);
+		void changeLandSubState(LAND_SUB_STATE next_state);
+		void initState();
 	public:
 		void init(ros::NodeHandle& nh);
   	Search_Plan_FSM(/* args */)
@@ -70,6 +84,7 @@ namespace auto_search
 		{
 
 		}
+		EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 	};
   
 }
--- src/auto_search/search_plan/src/search_plan_fsm.cpp
+++ src/auto_search/search_plan/src/search_plan_fsm.cpp
@@ -3,18 +3,45 @@
 
 namespace auto_search
 {
-std::string odom_Topic;
-double exec_Frequency;
+
 //执行起始阶段状态机
 void Search_Plan_FSM::execStartStage()
 {
-
+  switch (start_SubState)
+  {
+  case TAKE_OFF:
+  {
+    if(have_trigger)
+    {
+      changeStartSubState(FLY_TO_START);
+      ROS_INFO("Start substate switch: TAKEOFF ======> FLY_TO_START");
+    }
+    break;
+  }
+  case FLY_TO_START:
+  {
+    current_Target = search_StartPoint;
+    if(haveArrivedTarget())
+    {
+      changeStartSubState(WAIT_FOR_START);
+      ROS_INFO("Start substate switch: FLY_TO_START ======> WAIT_FOR_START");
+    }
+    break;
+  }
+  case WAIT_FOR_START:
+  {
+    //todo 所有无人机就绪后再进入搜索阶段
+    changeMainState(SEARCH_STAGE);
+    ROS_INFO("Main state switch: STARTING_AREA_STAGE ======> SEARCH_STAGE");
+    break;
+  }
+  }
 }
 
 //执行搜索阶段状态机
 void Search_Plan_FSM::execSearchStage()
 {
-  
+
 }
 
 //执行降落阶段状态机
@@ -38,7 +65,7 @@ void Search_Plan_FSM::execFSMCallback(const ros::TimerEvent &e)
     execSearchStage();
     break;
   }
-  case LANDING_STAGE:
+  case LAND_STAGE:
   {
     execLandStage();
     break;
@@ -46,6 +73,7 @@ void Search_Plan_FSM::execFSMCallback(const ros::TimerEvent &e)
   default:
     break;
   }
+  publishTarget();
   timer_FSM.start();
 }
 void Search_Plan_FSM::updateOdomCallback(const nav_msgs::OdometryConstPtr &msg)
@@ -54,24 +82,82 @@ void Search_Plan_FSM::updateOdomCallback(const nav_msgs::OdometryConstPtr &msg)
   current_Position[1] = msg->pose.pose.position.y;
   current_Position[2] = msg->pose.pose.position.z;
 }
+void Search_Plan_FSM::triggerCallback(const geometry_msgs::PoseStampedPtr &msg)
+{
+  have_trigger= true;
+  std::cout << "Triggered!" << std::endl;
+}
 void Search_Plan_FSM::publishTarget()
 {
-  if(current_Target != last_Target)
+  double dis = (current_Target - last_Target).norm();
+  if( dis >= 0.2)//新的目标点距离之前的目标点大于0.2m才发布新的目标点
   {
-
+    geometry_msgs::PoseStamped msg;
+    msg.header.stamp = ros::Time::now();
+    msg.pose.position.x = current_Position.x();
+    msg.pose.position.y = current_Position.y();
+    msg.pose.position.z = current_Position.z();
+    pub_Target.publish(msg);
+    
     last_Target = current_Target;
   }
-
 }
+
+void Search_Plan_FSM::changeMainState(MAIN_STATE next_state)
+{
+  main_State = next_state;
+}
+void Search_Plan_FSM::changeStartSubState(START_SUB_STATE next_state)
+{
+  start_SubState = next_state;
+}
+void Search_Plan_FSM::changeSearchSubState(SEARCH_SUB_STATE next_state)
+{
+  search_SubState = next_state;
+}
+void Search_Plan_FSM::changeLandSubState(LAND_SUB_STATE next_state)
+{
+  land_SubState = next_state;
+}
+
+bool Search_Plan_FSM::haveArrivedTarget()
+{
+  double distance = (current_Target - current_Position).norm();
+  if(distance <= arrive_Threshold)
+    return true;
+  return false;
+}
+//初始化各状态
+void Search_Plan_FSM::initState()
+{
+  main_State = STARTING_AREA_STAGE;
+  start_SubState = TAKE_OFF;
+  search_SubState = SEARCH_NUMS;
+  land_SubState = FLY_TO_NUMS;
+  ROS_INFO("Initail State: STARTING_AREA_STAGE<TAKE_OFF>");
+}
+
 void Search_Plan_FSM::init(ros::NodeHandle& nh)
 {
+  //var init
+  initState();
+  current_Target << 0.0, 0.0, 0.0;
+  last_Target << 0.0, 0.0, 0.0;
+
   //param
-  nh.param<std::string>("odom_topic", odom_Topic, "/vins/imu_prepagate");
+  nh.param<std::string>("odom_topic", odom_Topic, "/vins_fusion/imu_propagate");
   nh.param<double>("exec_frequency", exec_Frequency, 100);
+  nh.param<double>("arrive_threshold", arrive_Threshold, 0.3);
+  
+  nh.param<double>("search_startpoint_x", search_StartPoint.x(), 2);
+  nh.param<double>("search_startpoint_y", search_StartPoint.y(), 0.5);
+  nh.param<double>("search_startpoint_z", search_StartPoint.z(), 2);
+
   //sub
-  sub_Odom = nh.subscribe(odom_Topic, 100, &Search_Plan_FSM::updateOdomCallback, this);
+  sub_Odom = nh.subscribe(odom_Topic, 1, &Search_Plan_FSM::updateOdomCallback, this);
+  sub_Trigger = nh.subscribe("/traj_start_trigger", 1, &Search_Plan_FSM::triggerCallback, this);
   //pub
-  pub_Target = nh.advertise<geometry_msgs::PoseStamped>("/auto_search/pos_cmd", 50);
+  pub_Target = nh.advertise<geometry_msgs::PoseStamped>("/search_plan/pos_cmd", 50);
   //timer
   timer_FSM = nh.createTimer(ros::Duration(1/exec_Frequency), &Search_Plan_FSM::execFSMCallback, this);
 }
