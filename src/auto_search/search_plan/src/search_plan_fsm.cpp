bbcb3b30ca4461322c073a402d4ebbf40c219117
bkbk_b <1320832053@qq.com>
2024-07-24 12:33:46 +0800

    update search_plan

--- src/auto_search/search_plan/src/search_plan_fsm.cpp
+++ src/auto_search/search_plan/src/search_plan_fsm.cpp
@@ -14,13 +14,13 @@ void Search_Plan_FSM::execStartStage()
 //执行搜索阶段状态机
 void Search_Plan_FSM::execSearchStage()
 {
-
+  
 }
 
 //执行降落阶段状态机
 void Search_Plan_FSM::execLandStage()
 {
-  
+
 }
 //状态机执行函数
 void Search_Plan_FSM::execFSMCallback(const ros::TimerEvent &e)
@@ -53,6 +53,15 @@ void Search_Plan_FSM::updateOdomCallback(const nav_msgs::OdometryConstPtr &msg)
   current_Position[0] = msg->pose.pose.position.x;
   current_Position[1] = msg->pose.pose.position.y;
   current_Position[2] = msg->pose.pose.position.z;
+}
+void Search_Plan_FSM::publishTarget()
+{
+  if(current_Target != last_Target)
+  {
+
+    last_Target = current_Target;
+  }
+
 }
 void Search_Plan_FSM::init(ros::NodeHandle& nh)
 {
