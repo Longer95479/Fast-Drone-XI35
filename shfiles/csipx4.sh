031457ea585319d33ab0165c92992a2ab2ff4f9c
Longer95479 <371573369@qq.com>
2024-07-26 23:06:52 +0800

    add csipx4.sh

--- shfiles/csipx4.sh
+++ shfiles/csipx4.sh
@@ -1,5 +1,4 @@
 sudo chmod 777 /dev/ttyTHS0 & sleep 2;
-source jetson_csi_cam_ros_ws/devel/setup.bash & sleep 1;
 roslaunch jetson_csi_cam jetson_csi_cam.launch & sleep 2;
 roslaunch mavros px4.launch & sleep 10;
 roslaunch vins xi35_mono_downward.launch & sleep 4;
