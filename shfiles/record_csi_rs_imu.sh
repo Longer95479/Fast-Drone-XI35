960a8c5d14869439175a39d41196dc5aa6012e49
Longer95479 <371573369@qq.com>
2024-09-24 23:33:52 +0800

    add a cam for recording

--- shfiles/record_csi_rs_imu.sh
+++ shfiles/record_csi_rs_imu.sh
@@ -3,4 +3,5 @@ rosbag record --tcpnodelay \
 /camera/infra2/image_rect_raw \
 /camera/depth/image_rect_raw \
 /mavros/imu/data_raw \
-/csi_cam_0/image_raw
+/csi_cam_0/image_raw \
+/csi_cam_1/image_raw \
