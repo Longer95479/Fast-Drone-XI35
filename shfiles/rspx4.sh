c90f606486519062b870af54d342ad30a73e6021
Longer95479 <371573369@qq.com>
2024-12-02 18:54:58 +0800

    change /mavros/battery 0.5hz to 10hz

--- shfiles/rspx4.sh
+++ shfiles/rspx4.sh
@@ -1,5 +1,6 @@
 sudo chmod 777 /dev/ttyTHS0 & sleep 1;
-roslaunch realsense2_camera rs_camera.launch & sleep 4;
+roslaunch realsense2_camera rs_camera.launch & sleep 3;
 roslaunch mavros px4.launch & sleep 4;
+rosservice call /mavros/set_message_interval 147 10.0 & sleep 1;
 roslaunch vins fast_drone_250.launch & sleep 4;
 wait;
