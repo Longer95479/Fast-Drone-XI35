c1e6e32775da717076c1a01e240cdf9bd312d666
Longer95479 <371573369@qq.com>
2025-05-20 20:24:19 +0800

    make bat msg have higher freq (10HZ)

--- shfiles/rspx4_xp.sh
+++ shfiles/rspx4_xp.sh
@@ -1,6 +1,7 @@
 sudo chmod 777 /dev/ttyTHS0 & sleep 1;
-roslaunch realsense2_camera rs_camera.launch & sleep 4;
+roslaunch realsense2_camera rs_camera.launch & sleep 3;
 roslaunch mavros px4.launch & sleep 4;
+rosservice call /mavros/set_message_interval 147 10.0 & sleep 1;
 find ../ -type f -name 'fast_drone_250.yaml' -exec sed -i 's/use_external_front_end: 0/use_external_front_end: 1/1' {} \;
 roslaunch superpoint superpoint_frontend.launch & sleep 4;
 roslaunch vins fast_drone_250.launch & sleep 4;
