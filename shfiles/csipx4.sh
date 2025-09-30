b4d5c9705ca1c898a0ce23fdc870ee1d16e094d4
zhangrun <1139135655@qq.com>
2025-09-30 10:44:18 +0800

    ENH: Redirect ROS node logs to log directory
    Signed-off-by: zhangrun <1139135655@qq.com>

--- shfiles/csipx4.sh
+++ shfiles/csipx4.sh
@@ -1,4 +1,7 @@
-sudo chmod 777 /dev/ttyTHS0 & sleep 2;
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export ROS_LOG_DIR=${SCRIPT_DIR}/../log
+sudo chmod 777 /dev/ttyTHS0
+sleep 2;
 roslaunch jetson_csi_cam jetson_csi_cam.launch & sleep 2;
 roslaunch mavros px4.launch & sleep 10;
 roslaunch vins xi35_mono_downward.launch & sleep 4;
