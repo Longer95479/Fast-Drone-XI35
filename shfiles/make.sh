a702ceffda307de83c59904cb38379f0a70d90ce
zhangrun <1139135655@qq.com>
2025-09-30 09:41:42 +0800

    NEW: Add generate compile_command.json option at make.sh
    Signed-off-by: zhangrun <1139135655@qq.com>

--- shfiles/make.sh
+++ shfiles/make.sh
@@ -1,3 +1,6 @@
-
+# only build quadrotor_msgs package
 catkin_make --pkg quadrotor_msgs -DCMAKE_BUILD_TYPE=Release
+# build all packages
 catkin_make -DCMAKE_BUILD_TYPE=Release
+# build all packages with compile_commands.json
+catkin_make -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release
