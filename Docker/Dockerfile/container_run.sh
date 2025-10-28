fc9a06d4c315723c36913d5fb3bfd31744ee649c
zhangrun <1139135655@qq.com>
2025-10-28 15:48:50 +0800

    FIX: catkin_make failure in Docker env image build
    Signed-off-by: zhangrun <1139135655@qq.com>

--- Docker/Dockerfile/container_run.sh
+++ Docker/Dockerfile/container_run.sh
@@ -1,4 +1,13 @@
+# check if the container already exists
+if docker ps -a --format '{{.Names}}' | grep -qw fd_runtime; then
+    echo "Container 'fd_runtime' already exists. Exiting..."
+    exit 0
+fi
+
+# create data directory if it doesn't exist
 mkdir -p ${HOME}/Docker_Data
+
+# run the container
 docker run -itd --privileged=true --network host \
         --mount type=bind,source=${HOME}/Docker_Data,target=/root/data \
         --mount type=bind,source=/dev,target=/dev \
@@ -6,3 +15,26 @@ docker run -itd --privileged=true --network host \
         --runtime=nvidia --gpus all \
         --name fd_runtime \
         fastdronexi35:orin /bin/bash
+
+# LOG_FILE=/root/Fast-Drone-XI35/log/catkin_build.log
+
+# monitor the build log
+docker exec -i fd_runtime bash <<EOF
+LOG_FILE=/root/Fast-Drone-XI35/log/catkin_build.log
+
+while [ ! -f "\$LOG_FILE" ]; do
+    sleep 1
+done
+
+tail -n 0 -f "\$LOG_FILE" | while read line; do
+    echo "\$line"
+    if echo "\$line" | grep -q "catkin_make succeeded"; then
+        echo -e '\033[1;32m Build succeeded !!! \033[0m'
+        exit 0
+    fi
+    if echo "\$line" | grep -q "catkin_make failed"; then
+        echo -e '\033[1;31m Build failed !!! \033[0m'
+        exit 1
+    fi
+done
+EOF
--- Docker/container_init.sh
+++ Docker/container_init.sh
@@ -1,11 +1,17 @@
-#modify mavros
+ROS_VERSION=noetic
+OPENCV_VERSION=4.5.4
+FDRONE_WS=/root/Fast-Drone-XI35
+LOG_DIR=${FDRONE_WS}/log
+LOG_FILE=${LOG_DIR}/catkin_build.log
+
+# modify mavros
 sed -i 's|/dev/ttyACM0:57600|/dev/ttyTHS0:921600|1' /opt/ros/noetic/share/mavros/launch/px4.launch
 
-#LCM Config
+# LCM Config
 ifconfig wlan1 multicast
 route add -net 224.0.0.0 netmask 240.0.0.0 dev wlan1
 
-#start service
+# start service
 service ssh start
 /daemon/nvargus-daemon &
 
@@ -15,5 +21,22 @@ if ! grep -q "export ROS_LOG_DIR=" /root/.bashrc; then
     echo "Added ROS_LOG_DIR to /root/.bashrc"
 fi
 
-#login as root
+# Sourcing ROS environment
+source /opt/ros/${ROS_VERSION}/setup.bash
+source /root/cv_bridge${OPENCV_VERSION}_ws/devel/setup.bash
+
+# Build Fast-Drone-XI35 workspace
+cd ${FDRONE_WS}
+mkdir -p ${LOG_DIR}
+catkin_make 2>&1 | tee ${LOG_FILE}
+if [ ${PIPESTATUS[0]} -eq 0 ]; then
+    echo -e " catkin_make succeeded " | tee -a ${LOG_FILE}
+    if ! grep -q "${FDRONE_WS}/devel/setup.bash" /root/.bashrc; then
+        echo "source ${FDRONE_WS}/devel/setup.bash" >> /root/.bashrc
+    fi
+else
+    echo " catkin_make failed " | tee -a ${LOG_FILE}
+fi
+
+# login as root
 su - root
